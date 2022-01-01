8fc193348d1aec9c5e390901ee66653d6790129d
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-01 14:37:17 -0800

    Create WeatherManager.swift

---
+++ WeatherApp1/Managers/WeatherManager.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+    //func below uses new WWDC21 async await feature
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody
+    {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("566d38afd97a96811892be92a8056eb9")&units=metric")
+        else
+        {
+            fatalError("Missing URL")
+        }
+    
+        let urlRequest = URLRequest(url:url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else  { fatalError("Error fetching weather data")}
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
+    }
+}
+
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+    
+    struct CoordinatesResponse: Decodable
+    {
+        var lon: Double
+        var lat: Double
+    }
+    
+    struct WeatherResponse: Decodable
+    {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+    
+    struct MainResponse: Decodable
+    {
+        var temp: Double
+        var feel_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable
+    {
+        var speed: Double
+        var deg: Double
+    }
+}
